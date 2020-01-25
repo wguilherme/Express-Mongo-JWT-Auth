4c2f27bf8b44dff826dfa1a11d9eeb1143d51414
W. Guilherme <wguilhermecriativo@gmail.com>
2020-01-25 19:13:14 -0300

    [Update] Logout one or all tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/User')
+const auth = require('../middleware/auth')
 
 const router = express.Router()
 
@@ -32,6 +33,37 @@ router.post('/users/login', async(req, res) => {
         res.status(400).send(error)
     }
 
+})
+
+//Get current user
+router.get('/users/me', auth, async(req, res) => {
+    // View logged in user profile
+    res.send(req.user)
+})
+
+
+router.post('/users/me/logout', auth, async (req, res) => {
+    // Log user out of the application
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        await req.user.save()
+        res.json({message: 'User disconnected'})
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.post('/users/me/logoutall', auth, async(req, res) => {
+    // Log user out of all devices
+    try {
+        req.user.tokens.splice(0, req.user.tokens.length)
+        await req.user.save()
+        res.json({message: 'User disconnected on all devices'})
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 module.exports = router
