9b5bcc8d9856bfb713c52d8f0c6e94eec34aeb0f
W. Guilherme <ravok123@gmail.com>
2020-02-24 22:55:48 -0300

    [FIX] import express syntax

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import express from "express";
 import cors from "cors";
 import userRouter from "./routers/user";
 import itemRouter from "./routers/item";
 import customerRouter from "./routers/customer";
 import petRouter from "./routers/pet";
+
+const express = require("express");
 
 require("./db/db");
 
